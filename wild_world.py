7a5d406726b798e9f34b7a9da5e208534cf5248a
Sugs22 <96538310+Sugs22@users.noreply.github.com>
2021-12-22 18:24:12 +0100

    Update wild_world.py
    First data import from github

--- wild_world.py
+++ wild_world.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.write("Hola")
+import pandas as pd
+
+url = 'https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/country_data/Spain.csv'
+
+st.write("Import data vaccination")
+
+df = pd.read_csv(url)
+
+st.write(df.head(5))
